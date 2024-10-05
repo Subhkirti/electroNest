4f218d1fe07ea099a8d3f303c7b2e816653a7127
shubhkirti9 <shubhkirti21@navgurukul.org>
2024-10-06 01:11:02 +0530

    Update store.js

--- client/src/modules/customer/store/store.js
+++ client/src/modules/customer/store/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { applyMiddleware, legacy_createStore } from "redux";
 import { thunk } from "redux-thunk";
 import rootReducer from "./rootReducer";
 
