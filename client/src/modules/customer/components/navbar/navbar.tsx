d0b8e6e033ac4591938e51be7d65152f69bda4ec
shubhkirti9 <s.k@mahila.money>
2024-11-14 20:21:36 +0530

    Update navbar.tsx

--- client/src/modules/customer/components/navbar/navbar.tsx
+++ client/src/modules/customer/components/navbar/navbar.tsx
@@ -112,9 +112,9 @@ export default function Navbar() {
       {MobileSideNavbar()}
 
       <header className="relative bg-white">
-        <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+        {/* <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
           Get free delivery on orders over ₹1000
-        </p>
+        </p> */}
 
         {/* Web menu */}
         <nav aria-label="Top" className="mx-auto">
